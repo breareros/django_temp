9553b149cb766edd7930b41bdebae3d9e20fb51b
breareros <breareros@gmail.com>
2023-04-10 20:57:06 +0500

    Поиск по двум таблицам

--- apostil/static/apostil/js/search_table.js
+++ apostil/static/apostil/js/search_table.js
@@ -33,7 +33,7 @@ input.addEventListener('blur', function() {
 
 function searchTable() {
     // Получаем строку поиска и таблицу
-    var input, filter, table, tr, td, i, txtValue;
+    let input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("search");
     filter = input.value.toUpperCase();
     table = document.getElementById("ChanksTable");
@@ -57,5 +57,32 @@ function searchTable() {
     }
 }
 
+function searchTodayTable() {
+    // Получаем строку поиска и таблицу
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("TodayChanksTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Проходим по всем строкам таблицы и скрываем те, которые не соответствуют поиску
+    for (i = 0; i < tr.length; i++) {
+        // Проверяем по каждой ячейке в строке
+        for (j = 0; j < tr[i].cells.length; j++) {
+            td = tr[i].cells[j];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                    break; // Если есть хотя бы одно совпадение, то показываем строку
+                } else {
+                    tr[i].style.display = "none"; // Иначе скрываем
+                }
+            }
+        }
+    }
+}
+
 // Добавляем обработчик события на изменение значения input
 input.addEventListener('input', searchTable);
+input.addEventListener('input', searchTodayTable);
